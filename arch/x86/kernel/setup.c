0e5d422ad32ee6b13477b1b402aeb4feeb21a32e
Chen Zhuo <sagazchen@tencent.com>
2022-02-10 16:42:14 +0800

    mm: support direct memory reservation
    Introduce 'dmem=' to reserve system memory for DMEM (direct memory),
    comparing with 'mem=' and 'memmap', it reserves memory based on the
    topology of NUMA, for the detailed info, please refer to
    kernel-parameters.txt
    
    Signed-off-by: Chen Zhuo <sagazchen@tencent.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -76,6 +76,7 @@
 
 #include <linux/usb/xhci-dbgp.h>
 #include <video/edid.h>
+#include <linux/dmem.h>
 
 #include <asm/mtrr.h>
 #include <asm/apic.h>
@@ -1050,6 +1051,8 @@ void __init setup_arch(char **cmdline_p)
 
 	initmem_init();
 	dma_contiguous_reserve(max_pfn_mapped << PAGE_SHIFT);
+
+	dmem_reserve_init();
 
 	/*
 	 * Reserve memory for crash kernel after SRAT is parsed so that it
